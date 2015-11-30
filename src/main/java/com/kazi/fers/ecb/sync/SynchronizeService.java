979d586a3b12d59cd5bc88e86b1a7fbd89f66e67
kazi87 <xk.zielinski@gmail.com>
2015-11-30 23:56:08 +0100

    ECBSynchronization Service with fixed configuration
    TODO: configuration should be extracted...

--- src/main/java/com/kazi/fers/ecb/sync/SynchronizeService.java
+++
@@ -1,8 +0,0 @@
-package com.kazi.fers.ecb.sync;
-
-/**
- * Responsible for circular updates from ECB servers...
- * TODO: change the class name...
- */
-public class SynchronizeService {
-}
---
+++ src/main/java/com/kazi/fers/sync/ECBSynchronizeService.java
@@ -0,0 +1,66 @@
+package com.kazi.fers.sync;
+
+import com.kazi.fers.cache.ExRateCache;
+import com.kazi.fers.ecb.ECBClientService;
+import com.kazi.fers.model.ecb.CubeContainer;
+import com.kazi.fers.model.ecb.CubeEntry;
+import com.kazi.fers.model.ecb.DayCube;
+import com.kazi.fers.model.fer.ExRate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+
+import javax.annotation.PostConstruct;
+import java.time.LocalDate;
+import java.util.Date;
+
+/**
+ * Responsible for circular updates from ECB servers...
+ */
+@Configuration
+@EnableScheduling
+public class ECBSynchronizeService {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(ECBSynchronizeService.class);
+
+    @Autowired
+    private ECBClientService ecbClientService;
+
+    @Autowired
+    private ExRateCache cache;
+
+    @PostConstruct
+    private void initialSynchronization() {
+        LOGGER.info("Initial FERCache synchronization...");
+        synchronize();
+    }
+
+    @Scheduled(cron = "0 0/1 * * * ?")
+    public void synchronize() {
+        LOGGER.info("Synchronize service start at: " + new Date());
+        CubeContainer container = ecbClientService.getLast90Days();
+        if (container != null) {
+            parseCubeContainer(container);
+            LOGGER.warn("Synchronized local cache with ECB server.");
+        } else {
+            LOGGER.warn("Could not synchronize data with ECB server!");
+        }
+    }
+
+    private void parseCubeContainer(CubeContainer container) {
+        cache.flush();
+        int i = 0;
+        for (DayCube dc : container.getDayCubes()) {
+            LocalDate date = dc.getTime();
+            for (CubeEntry ce : dc.getEntries()) {
+                ExRate exRate = new ExRate(ce.getCurrency(), ce.getRate(), date);
+                cache.updateExRate(exRate);
+                i++;
+            }
+        }
+        LOGGER.debug("Updated cache with " + i + " entries.");
+    }
+}
