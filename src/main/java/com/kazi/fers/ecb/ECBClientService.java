aff4df9436574027d971d67950a0ab028d7d6f17
kazi87 <xk.zielinski@gmail.com>
2015-11-30 21:53:09 +0100

    ECBClientService + tests

--- src/main/java/com/kazi/fers/ecb/ECBClientService.java
+++ src/main/java/com/kazi/fers/ecb/ECBClientService.java
@@ -1,8 +1,36 @@
 package com.kazi.fers.ecb;
 
+import com.kazi.fers.model.ecb.CubeContainer;
+import com.kazi.fers.model.ecb.Envelope;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
 /**
  * The ECB client service.
+ * TODO: TEST!
  */
+@Service
 public class ECBClientService {
-    //  Add methods...
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(ECBClientService.class);
+
+    private final static String ECB_90_DAYS_URL = "https://www.ecb.europa.eu/stats/eurofxref/eurofxref-hist-90d.xml";
+
+    private final RestTemplate template = new RestTemplate();
+
+    public CubeContainer getLast90Days() {
+        try {
+            Envelope envelope = template.getForObject(ECB_90_DAYS_URL, Envelope.class);
+            if (envelope != null) {
+                LOGGER.info("Read CubeContainer from remote service...");
+                return envelope.getCubeContainer();
+            }
+        } catch (RestClientException e) {
+            throw new IllegalStateException("Can not read data from the url: " + ECB_90_DAYS_URL, e);
+        }
+        return null;
+    }
 }
